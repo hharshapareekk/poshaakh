9cf1aac8e3f027defb3714c07881fb671833e920
Harsha Pareek <harshapareek14@gmail.com>
2024-07-15 19:50:46 +0530

    pinfluence

--- src/components/Pinfluence.js
+++ src/components/Pinfluence.js
@@ -1,58 +1,39 @@
 import { useState } from "react";
+import image1 from "../images/pin1.png";
+import image2 from "../images/pin2.png";
+import image3 from "../images/pin3.png";
 import cover from "../images/pinfluence-cover.png";
-import image1 from "../images/sustainable-1.png";
-import image2 from "../images/sustainable-2.png";
-import image3 from "../images/sustainable-3.png";
 import PinfluenceItem from "./PinflueneceItem";
-
+import imagee from "../images/generate.png";
 const images = [
   {
     id: 1,
     src: image1,
-    title: "Garment Resale",
+    title: "Crop Top with Baggy Jeans",
     category:
-      "Bring your unwanted products to us. We’ll make sure they get reworn, reused or recycled. Earn 20 points and a 15% off digital voucher every 1 kg.",
+      "You're a master of effortless style, and your go-to combo of baggy jeans and a crop top is the perfect proof. ",
   },
   {
     id: 2,
     src: image2,
-    title: "Eco-Friendly Packaging",
+    title: "Monochrome Outfits",
     category:
-      "Choose eco-friendly packaging for your online orders. Opt for reusable or biodegradable packaging and earn 15 points per order. Make a greener choice with every purchase.",
+      "You love monochrome outfits, finding simplicity in the single-color palette. A monochrome look is your go-to for a chic, easy style.",
   },
   {
     id: 3,
     src: image3,
-    title: "Tree Planting Initiative",
+    title: "Kurti And Jeans",
     category:
-      "For every purchase above INR 1000, we plant a tree in your name. Earn 30 points and a special tree planting certificate. Help us reforest and restore natural habitats.",
+      "You love mixing and matching different styles, pairing a traditional cotton kurti with baggy jeans for a unique look. Adding bold jhumkas to the mix, you create a fascinating fusion of aesthetics that's all your own.",
   },
 ];
 
 const Pinfluence = () => {
-  const [containers, setContainers] = useState([]);
+  const [generatedImage, setGeneratedImage] = useState(null);
 
   const handleGenerate = () => {
-    const newContainers = [];
-    for (let i = 0; i < 6; i++) {
-      newContainers.push(
-        <div
-          key={i}
-          className="container"
-          style={{
-            width: "250px",
-            height: "150px",
-            backgroundColor: "#f7f7f7",
-            border: "1px solid #ddd",
-            padding: "20px",
-            margin: "10px",
-          }}
-        >
-          Container {i + 1}
-        </div>
-      );
-    }
-    setContainers(newContainers);
+    setGeneratedImage(imagee);
   };
 
   return (
@@ -78,42 +59,47 @@ const Pinfluence = () => {
           marginBottom: "30px",
         }}
       />
-      <button
-        onClick={handleGenerate}
-        style={{
-          backgroundColor: "#d53f41",
-          color: "#FFFFFF",
-          padding: "10px",
-          borderRadius: "20px",
-          border: "none",
-          cursor: "pointer",
-          fontSize: "20px",
-          fontWeight: "bold",
-          marginBottom: "20px",
-          marginLeft: "650px",
-          width: "250px",
-          height: "50px",
-          letterSpacing: "4px",
-        }}
-      >
-        Generate
-      </button>
       <div
-        className="container-wrapper"
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-    gridTemplateRows: "repeat(2, 1fr)",
-          gap: "20px",
-        }}
-      >
-        {containers}
-      </div>
-      <h2> Your Personalized Aesthetic Learnings</h2>
+  style={{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  }}
+>
+  <button
+    onClick={handleGenerate}
+    style={{
+      backgroundColor: "#d53f41",
+      color: "#FFFFFF",
+      padding: "10px",
+      borderRadius: "20px",
+      border: "none",
+      cursor: "pointer",
+      fontSize: "20px",
+      fontWeight: "bold",
+      marginBottom: "10px",
+      width: "250px",
+      height: "50px",
+      letterSpacing: "4px",
+    }}
+  >
+    Generate
+  </button>
+  {generatedImage && (
+          <img
+            src={generatedImage}
+            style={{ width: "1050px", height: "550px", margin: "10px" }}
+          />
+        )}
+</div>
+      <h2 className="pinfluence-heading">
+        {" "}
+        Your Personalized Aesthetic Learnings
+      </h2>
       <div className="sustainable-wrapper">
         {images.map((image) => (
           <PinfluenceItem key={image.id} image={image} />
-
         ))}
       </div>
     </section>
